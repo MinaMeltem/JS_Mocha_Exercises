d456aa65997d3fa260eda4b34bfecbe7119db4d7
Mel <meltem@gmail.com>
2019-09-26 19:54:27 -0400

    Update union.js

--- src/callbacks_functions/union.js
+++ src/callbacks_functions/union.js
@@ -5,6 +5,16 @@
 * Preserve the order of the elements starting from the first element of the first input array
 * */
 
+function union(list){
+    let output = [] ;
+    for (let array = 0; array < list.length;  array++){
+        for(let item in list[array]) {
+           output.push(list[array][item]);
+        }
+    }
+    return unique(output);
+}
+
 // Callback
 function unique(arr){
     let output = [];
@@ -16,19 +26,9 @@ function unique(arr){
     return output;
 }
 
-function union(list){
-    let output = [] ;
-    for (let array = 0; array < list.length;  array++){
-        for(let item in list[array]) {
-           output.push(list[array][item]);
-        }
-    }
-    return unique(output);
-}
 
 
-
-// Test
+// TEST
 const arr1 = [5, 10, 15, 20];
 const arr2 = [15, 88, 1, 5, 7];
 const arr3 = [1, 10, 15, 5, 20];
