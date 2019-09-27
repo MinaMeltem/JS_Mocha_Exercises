e88eeeb00e7d9e4bc240a1814f5424443279ff3a
Mel <meltem@gmail.com>
2019-09-26 20:00:05 -0400

    Update majority.js

--- src/callbacks_functions/majority.js
+++ src/callbacks_functions/majority.js
@@ -1,9 +1,10 @@
 /*
-* Create a function majority that accepts an array and a callback. The callback will return either true or false.
+* QUESTION: Create a function majority that accepts an array and a callback. The callback will return either true or false.
 * majority will iterate through the array and perform the callback on each element until it can be determined if
 * the majority of the return values from the callback are true. If the number of true returns is equal to the number
 * of false returns, majority should return false.
-* */
+* 
+*/
 
 function mjrty(arr, callback) {
     let f = 0;
