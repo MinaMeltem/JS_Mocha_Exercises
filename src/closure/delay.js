909eac71a8db5260edfde291b2c4e1eff2810ee2
MinaMeltem <meltem@gmail.com>
2019-09-26 22:05:22 -0400

    Add delay.js

---
+++ src/closure/delay.js
@@ -0,0 +1,5 @@
+/*
+* QUESTION: Write a function delay that accepts two arguments, a callback and the wait time in milliseconds.
+* Delay should return a function that, when invoked waits for the specified amount of time before executing.
+* (HINT - research setTimeout();)
+* */
